a3ef3b87f1bf240053981f6b67c4dc44c9624b91
Pedro Gonzalez Ferrandez <pedro.gonzalez@square1.io>
2017-10-06 19:48:27 +0200

    add coin behaviour

--- A Maze/Assets/UdacityVR/Scripts/Coin.cs
+++ A Maze/Assets/UdacityVR/Scripts/Coin.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class Coin : MonoBehaviour 
 {
     //Create a reference to the CoinPoofPrefab
-	public Object coinPoofPrefab;
+	public GameObject coinPoof;
 
     public void OnCoinClicked() {
         // Instantiate the CoinPoof Prefab where this coin is located
-        
+		
+		Instantiate (coinPoof, transform.position, Quaternion.Euler (-90.0f, 0, 0));
+
 		// Make sure the poof animates vertically
 
         // Destroy this coin. Check the Unity documentation on how to use Destroy
-		Destroy (gameObject);
+		Destroy(gameObject);
     }
 
 }
