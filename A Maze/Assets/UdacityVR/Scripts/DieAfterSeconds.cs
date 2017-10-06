38dcc70e92db5fee4e1455c82ace3a9fea6be412
Pedro Gonzalez Ferrandez <pedro.gonzalez@square1.io>
2017-10-06 20:05:23 +0200

    add coin sound

--- A Maze/Assets/UdacityVR/Scripts/DieAfterSeconds.cs
+++ A Maze/Assets/UdacityVR/Scripts/DieAfterSeconds.cs
@@ -2,7 +2,11 @@
 using System.Collections;
 
 public class DieAfterSeconds : MonoBehaviour {
+
     IEnumerator Start () {
+	
+		GetComponent<AudioSource> ().Play ();
+
         yield return new WaitForSeconds(2.0f);
         Destroy(gameObject);
 	}
